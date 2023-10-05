3ca718917a503ef606c448538a8d858404405f10
aluwaye-noah <a.yusuf1@gmail.com>
2023-10-05 07:11:02 -0700

    code 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+	    public static List<int> CreatePrint(int size)
+		        {
+				        List<int> list;
+					        string sep = "";
