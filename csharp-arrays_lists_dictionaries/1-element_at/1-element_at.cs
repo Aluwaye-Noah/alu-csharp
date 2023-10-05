cedef78324011da22a295ec801cdb5acaad07da1
aluwaye-noah <a.yusuf1@gmail.com>
2023-10-05 05:25:07 -0700

    list directories

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Array
+{
+	    public static int elementAt(int[] array, int index)
+		        {
+				        if (index < 0 || index >= array.Length)
+						        {
+								            Console.WriteLine("Index out of range");
